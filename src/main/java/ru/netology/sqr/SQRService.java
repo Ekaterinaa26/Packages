ea63016158fcd88fbb47e637d41a270172dca5a6
Екатерина <kgluhova49@gmail.com>
2023-02-17 18:33:36 +0200

    Initial commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,22 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public SQRService() {
+    }
+
+    public int calc(int lowLimit, int highLimit) {
+        int counter = 0;
+
+        for (int i = 10; i <= 99; i++) {
+            int ii = i * i;
+            if (ii >= lowLimit) {
+                if (ii <= highLimit) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @Test
+    public void testCalcWhenFewSquaresFound() {
+        SQRService service = new SQRService();
+
+
+        int actual = service.calc(  200, 300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
